23ba37599bfc7cff259e955c789faacd1f059e61
Kai Barnes <barnes.kai@gmail.com>
2018-04-17 21:59:52 +0900

    destructure props

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 
-const PortfolioItem = props => (
+const PortfolioItem = ({ image, title, link, skills, description, github }) => (
   <div className="PortfolioItem">
-    <img src={props.image} alt={props.title} className="PortfolioItem__image" />
+    <img src={image} alt={title} className="PortfolioItem__image" />
     <div className="PortfolioItem__text">
       <h3 className="PortfolioItem__header heading">
-        <a href={props.link}>{props.title}</a>
+        <a href={link}>{title}</a>
       </h3>
-      <p className="PortfolioItem__skills">I used {props.skills}</p>
-      <p className="PortfolioItem__description">{props.description}</p>
-      {props.link ? (
-        <a className="PortfolioItem__link" href={props.link}>
+      <p className="PortfolioItem__skills">I used {skills}</p>
+      <p className="PortfolioItem__description">{description}</p>
+      {/* Only render if a link prop is passed */}
+      {link ? (
+        <a className="PortfolioItem__link" href={link}>
           Visit Site
         </a>
       ) : null}
-      {props.github ? (
-        <a className="PortfolioItem__link" href={props.github}>
+      {/* Only render if a github prop is passed */}
+      {github ? (
+        <a className="PortfolioItem__link" href={github}>
           View on Github
         </a>
       ) : null}
